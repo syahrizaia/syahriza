6a14f51e7d7a2d079c690fdb62d07c9670c3879d
syahrizaia <syahrizaalsistani@gmail.com>
2023-12-12 15:21:07 +0700

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <head>
+        <Head />
+        <link rel="icon" href="/IMG_20231005_120702.jpg" />
+        <title>Syahriza IA</title>
+      </head>
+      <body className='bg-black'>
         <Main />
         <NextScript />
       </body>
